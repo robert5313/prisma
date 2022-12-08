0e3edd2ce210dad0a7da0b6134584884b255a23c
robert5313 <kiptoorobert@etech.ac.ke>
2022-12-08 10:40:15 +0300

    My second commit

---
+++ file1/prisma/seed.ts
@@ -0,0 +1,28 @@
+// prisma/seed.ts
+
+import { PrismaClient } from '@prisma/client';
+
+import { links } from '../data/links';
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      email: `texteljunior@gmail.com`,
+      role: 'USER',
+    },
+  })
+
+  await prisma.link.createMany({
+    data: links,
+  })
+}
+
+main()
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
